75f8a977b2d8ca6886dc9a15da483f77e40d1df0
iamshiv007 <iamshiv20032003@gmail.com>
2023-07-17 21:20:57 +0530

    some modifications

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import App from './App';
 import { Provider } from 'react-redux';
 import { store } from './store'
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 
+axios.defaults.withCredentials = true
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
